dbf9bf43f7b43b75c4c3b73c09817e82760553e6
Shivam Srivastav <107093652+mrsrivastavji@users.noreply.github.com>
2025-01-30 10:15:12 +0000

    container config done

---
+++ basic/variable.js
@@ -0,0 +1,9 @@
+const accountId=235476
+let accountEmail="vgfdt@gmdil.vpm"
+var accountPassword="123547"
+accountCity="gnhgfd"
+accountState="fgdsert";
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
